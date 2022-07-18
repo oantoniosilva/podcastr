e70a7818fe7a53603039c1900b9db2d4b2cacdaf
Antonio <antoniopsilva083@gmail.com>
2022-07-18 15:13:13 -0300

    feat: create player component and styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,21 @@
 import '../styles/globals.scss'
+
 import type { AppProps } from 'next/app'
+import { Header } from '../components/Header'
+import { Player } from '../components/Player'
+
+import styles from '../styles/app.module.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={styles.wrapper}>
+      <main>
+        <Header />
+        <Component {...pageProps} />
+      </main>
+      <Player />
+    </div>
+  )
 }
 
 export default MyApp
